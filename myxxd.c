ba4152863d22d45ad1473a87ede7d3729ee828f6
Chris Mero <entracus@xwing.cs.byu.edu>
2020-09-18 16:28:23 -0600

    please

---
+++ myxxd.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+#define BAD_NUMBER_ARGS 1
+
+/**
+ * Parses the command line.
+ *
+ * argc: the number of items on the command line (and length of the
+ *       argv array) including the executable
+ * argv: the array of arguments as strings (char* array)
+ * bits: the integer value is set to TRUE if bits output indicated
+ *       outherwise FALSE for hex output
+ *
+ * returns the input file pointer (FILE*)
+ **/
+FILE *parseCommandLine(int argc, char **argv, int *bits) {
+  if (argc > 2) {
+    printf("Usage: %s [-b|-bits]\n", argv[0]);
+    exit(BAD_NUMBER_ARGS);
+  }
+
+  if (argc == 2 &&
+      (strcmp(argv[1], "-b") == 0 || strcmp(argv[1], "-bits") == 0)) {
+    *bits = TRUE;
+  } else {
+    *bits = FALSE;
+  }
+
+  return stdin;
+}
+
+/**
+ * Writes data to stdout in hexadecimal.
+ *
+ * See myxxd.md for details.
+ *
+ * data: an array of no more than 16 characters
+ * size: the size of the array
+ **/
+void printDataAsHex(unsigned char *data, size_t size) {
+  printf(" ");
+  unsigned char hexString[40];
+  int hexCount = 0;
+  for(int i  = 0; i <  size; ++i){
+    if(i == 0) {
+       sprintf((char*)(hexString+hexCount),"%02X", data[i]);
+       hexCount += 2;
+    }
+    else if(i % 2 == 1){
+      sprintf((char*)(hexString+hexCount),"%02X%s", data[i], " ");
+      hexCount += 3;
+    }
+    else {
+      sprintf((char*)(hexString+hexCount),"%02X", data[i]);
+      hexCount += 2;
+    }
+  }
+  printf("%-40s ", hexString);
+}
+
+/**
+ * Writes data to stdout as characters.
+ *
+ * See myxxd.md for details.
+ *
+ * data: an array of no more than 16 characters
+ * size: the size of the array
+ **/
+void printDataAsChars(unsigned char *data, size_t size) {
+  for(int i = 0; i < size; ++i){
+    printf("%c", data[i]);
+  }
+}
+
+void readAndPrintInputAsHex(FILE *input) {
+  unsigned char data[16];
+  int numBytesRead = fread(data, 1, 16, input);
+  unsigned int offset = 0;
+  while (numBytesRead != 0) {
+    printf("%08x:", offset);
+    offset += numBytesRead;
+    printDataAsHex(data, numBytesRead);
+    printf("  ");
+    printDataAsChars(data, numBytesRead);
+    printf("\n");
+    numBytesRead = fread(data, 1, 16, input);
+  }
+}
+
+/**
+ * Bits output for xxd.
+ *
+ * See myxxd.md for details.
+ *
+ * input: input stream
+ **/
+void readAndPrintInputAsBits(FILE *input) {
+  unsigned char data[6];
+  int numBytesRead = fread(data, 1, 6, input);
+  unsigned int offset = 0;
+  while (numBytesRead != 0) {
+    printf("%08x: ", offset);
+    offset += numBytesRead;
+    for(int i = 0; i < numBytesRead; ++i){
+      for(int j = 7; j >= 0; --j){
+        putchar((data[i] & (1 << j)) ? '1' : '0');
+      }
+      printf(" ");
+    }
+    printf(" ");
+    if(numBytesRead < 6) {
+      int s = (6 - numBytesRead) * 9;
+      printf("%*s", s, " ");
+    }
+    printDataAsChars(data, numBytesRead);
+    printf("\n");
+    numBytesRead = fread(data, 1, 6, input);
+  }
+}
+
+int main(int argc, char **argv) {
+  int bits = FALSE;
+  FILE *input = parseCommandLine(argc, argv, &bits);
+
+  if (bits == FALSE) {
+    readAndPrintInputAsHex(input);
+  } else {
+    readAndPrintInputAsBits(input);
+  }
+  return 0;
+}
